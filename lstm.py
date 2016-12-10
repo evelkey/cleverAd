6c1e23877eace72b12ef346079d6554b72a52bda
Geza Velkey <velkey@fred.ilab.sztaki.hu>
2016-12-10 10:02:25 +0100

    learn

--- lstm.py
+++ lstm.py
@@ -6,23 +6,8 @@ from tensorflow.contrib import learn as tflearn
 from tensorflow.contrib import layers as tflayers
 
 
-def x_sin(x):
-    return x * np.sin(x)
-
-
-def sin_cos(x):
-    return pd.DataFrame(dict(a=np.sin(x), b=np.cos(x)), index=x)
-
-
 def rnn_data(data, time_steps, labels=False):
-    """
-    creates new data frame based on previous observation
-      * example:
-        l = [1, 2, 3, 4, 5]
-        time_steps = 2
-        -> labels == False [[1, 2], [2, 3], [3, 4]]
-        -> labels == True [2, 3, 4, 5]
-    """
+
     rnn_df = []
     for i in range(len(data) - time_steps):
         if labels:
@@ -38,9 +23,7 @@ def rnn_data(data, time_steps, labels=False):
 
 
 def split_data(data, val_size=0.1, test_size=0.1):
-    """
-    splits data to training, validation and testing parts
-    """
+
     ntest = int(round(len(data) * (1 - test_size)))
     nval = int(round(len(data.iloc[:ntest]) * (1 - val_size)))
 
@@ -50,10 +33,7 @@ def split_data(data, val_size=0.1, test_size=0.1):
 
 
 def prepare_data(data, time_steps, labels=False, val_size=0.1, test_size=0.1):
-    """
-    Given the number of `time_steps` and some data,
-    prepares training, validation and test data for an lstm cell.
-    """
+
     df_train, df_val, df_test = split_data(data, val_size, test_size)
     return (rnn_data(df_train, time_steps, labels=labels),
             rnn_data(df_val, time_steps, labels=labels),
@@ -70,9 +50,9 @@ def load_csvdata(rawdata, time_steps, seperate=False):
     return dict(train=train_x, val=val_x, test=test_x), dict(train=train_y, val=val_y, test=test_y)
 
 
-def generate_data(fct, x, time_steps, seperate=False):
-    """generates data with based on a function fct"""
-    data = fct(x)
+def generate_data(y, x, time_steps, seperate=False):
+
+    data = y
     if not isinstance(data, pd.DataFrame):
         data = pd.DataFrame(data)
     train_x, val_x, test_x = prepare_data(data['a'] if seperate else data, time_steps)
@@ -81,17 +61,7 @@ def generate_data(fct, x, time_steps, seperate=False):
 
 
 def lstm_model(num_units, rnn_layers, dense_layers=None, learning_rate=0.1, optimizer='Adagrad'):
-    """
-    Creates a deep model based on:
-        * stacked lstm cells
-        * an optional dense layers
-    :param num_units: the size of the cells.
-    :param rnn_layers: list of int or dict
-                         * list of int: the steps used to instantiate the `BasicLSTMCell` cell
-                         * list of dict: [{steps: int, keep_prob: int}, ...]
-    :param dense_layers: list of nodes for each layer
-    :return: the model definition
-    """
+
 
     def lstm_cells(layers):
         if isinstance(layers[0], dict):
